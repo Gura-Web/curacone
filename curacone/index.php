e864b0322eceab99773f45ca2b96580f1c8c553f
三木 百花 <miki.momoka.0317@gmail.com>
2019-05-02 10:20:12 +0900

    作品一覧モーダルウィンドウ スタイル

--- curacone/index.php
+++ curacone/index.php
@@ -7,7 +7,7 @@
   <meta name="description" content="ページの説明">
   <title>Cura&nbsp;Cone｜作品一覧</title>
   <link rel="stylesheet" href="css/style.css">
-  <link href="https://fonts.googleapis.com/css?family=Asap|M+PLUS+1p" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Asap:400,700|M+PLUS+1p:400,700" rel="stylesheet">
 </head>
 <body>
   <!-- <header>
@@ -78,55 +78,58 @@
       </div>
       </div>
 
-      <!-- <div class="modal-wrap">
-        <div calss="modal">
-          <p class="modal__thumb"><img src="img/img-sample.png" alt=""></p>
+      <div class="modal-wrap">
+        <button class="btn-modal-close"></button>
+        <div class="modal">
+          <p class="modal__thumb"><img src="img/img-modal.png" alt=""></p>
           <div class="modal__info">
-            <div calss="work">
-              <h4 calss="work__title">タイポグラフィアート</h4>
-              <p calss="work__url"><a href="">https://dribbble.com/shots/6355293-Travel-Page-Animation</a></p>
-              <p calss="work__text">
+            <div class="work">
+              <h4 class="work__title">タイポグラフィアート</h4>
+              <p class="work__url"><a href="">https://dribbble.com/shots/6355293-Travel-Page-Animation</a></p>
+              <p class="work__text">
                 この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。<br><br>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。
               </p>
             </div>
             <div class="creator">
-              <div class="creator__title">
-                <span>制作者</span>
-                <p>Creator</p>
+              <div class="creator-in">
+                <div class="creator-in__title">
+                  <span>制作者</span>
+                  <p>Creator</p>
+                </div>
+                <div class="creator-in__prof">
+                  <p class=""><img src="img/icon-sample.png" alt=""></p>
+                  <p class="prof__name">三木　ケンケン</p>
+                  <p class="prof__text">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</p>
+                </div>
+                <button class="btn btn--blue creator-in__btn">プロフィールを見る</button>
               </div>
-              <div class="creator__prof">
-                <p class=""><img src="img/icon-sample.png" alt=""></p>
-                <p class="prof__name">三木　ケンケン</p>
-                <p class="prof__text">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</p>
-              </div>
-              <button class="btn btn--blue">プロフィールを見る</button>
             </div>
           </div>
           <div class="modal__comment">
             <h4>みんなのコメント</h4>
             <ul class="comment">
-              <li class="commnet__list">
-                <p class="commnet-list__icon"><img src="img/icon-sample.png" alt=""></p>
-                <p class="commnet-list__text">聖書第１章、 はじめに神は天と地とを創造された。 2 地は形なく、むなしく、やみが淵のおもてにあり、神の霊が水のおもてをおおっていた。</p>
+              <li class="comment__list">
+                <p class="comment-list__icon"><img src="img/icon-sample.png" alt=""><span>砂糖 大地</span></p>
+                <p class="comment-list__text">聖書第１章、 はじめに神は天と地とを創造された。 2 地は形なく、むなしく、やみが淵のおもてにあり、神の霊が水のおもてをおおっていた。</p>
               </li>
-              <li class="commnet__list">
-                <p class="commnet-list__icon"><img src="img/icon-sample.png" alt=""></p>
-                <p class="commnet-list__text">聖書第１章、 はじめに神は天と地とを創造された。 2 地は形なく、むなしく、やみが淵のおもてにあり、神の霊が水のおもてをおおっていた。</p>
+              <li class="comment__list">
+                <p class="comment-list__icon"><img src="img/icon-sample.png" alt=""><span>砂糖 大地</span></p>
+                <p class="comment-list__text">聖書第１章、 はじめに神は天と地とを創造された。 2 地は形なく、むなしく、やみが淵のおもてにあり、神の霊が水のおもてをおおっていた。</p>
                 <ul class="reply">
                   <li class="reply__list">
-                    <p class="commnet-list__icon"><img src="img/icon-sample.png" alt=""></p>
-                    <p class="commnet-list__text">聖書第１章、 はじめに神は天と地とを創造された。 2 地は形なく、むなしく、やみが淵のおもてにあり、神の霊が水のおもてをおおっていた。</p>
+                    <p class="comment-list__icon"><img src="img/icon-sample.png" alt=""><span>砂糖 大地</span></p>
+                    <p class="comment-list__text">聖書第１章、 はじめに神は天と地とを創造された。 2 地は形なく、むなしく、やみが淵のおもてにあり、神の霊が水のおもてをおおっていた。</p>
                   </li>
                 </ul>
               </li>
             </ul>
             <div class="comment-submit">
               <h5 class="comment-submit__title">コメントをしてみよう！</h5>
-              <p class="comment-submit__bar"><img src="img/icon-sample.png" alt=""><input type="text"></p>
+              <p class="comment-submit__bar"><img src="img/icon-sample.png" alt=""><input type="text" placeholder="作品に対しての感想など..."></p>
             </div>
           </div>
         </div>
-      </div> -->
+      </div>
       
       <ul class="workList">
         <li class="work">
@@ -196,18 +199,18 @@
           </div>
         </li>
       </ul>
-      <button class="btn btn-view">viermore</button>
+      <!-- <button class="btn btn-view">viermore</button>
       <button class="btn-add">
         <div class="btn-add__deco">
           <span></span>
           <span></span>
         </div>
         <span class="btn-add__txt">作品を投稿する</span>
-      </button>
+      </button> -->
     </main>
-  <footer class="foot">
+  <!-- <footer class="foot">
     &copy; 2019 curacone.
-  </footer>
+  </footer> -->
   
 </body>
 </html>
